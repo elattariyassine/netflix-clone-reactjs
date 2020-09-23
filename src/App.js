823c253f75903bfa7e4563fead5da1cafece2d0a
ELATTARIYassine <yassineattari64@gmail.com>
2020-09-23 22:08:14 +0000

    Added banner & random movie banner & app hear wip 70%

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./App.css";
 import Row from "./components/row/Row";
 import requests from "./request";
+import Banner from "./components/banner/Banner";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      {/* {navbar} */}
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
+        isLargeRow
         fetchUrl={requests.fetchNetflixOriginals}
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -4,13 +4,13 @@ import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-const Row = ({ title, fetchUrl }) => {
+const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      console.log(request.data.results);
+      // console.log(request.data.results);
       setMovies(request.data.results);
       return request;
     }
@@ -24,9 +24,11 @@ const Row = ({ title, fetchUrl }) => {
         {movies.map((movie) => (
           <img
             key={movie.id}
-            src={`${base_url}${movie.poster_path}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
-            className="row__poster"
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
           />
         ))}
       </div>
